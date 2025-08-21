8a5243f87e00582959cad1e95df315b8748fe3c9
leonaiyem <mdnaiyem.islam.10@gmail.com>
2025-08-21 16:44:24 +0600

    repo changed

---
+++ resources/views/webview/partials/links/header.blade.php
@@ -0,0 +1,34 @@
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+    integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/fontawesome.min.css"
+    integrity="sha512-R+xPS2VPCAFvLRy+I4PgbwkWjw1z5B5gNDYgJN5LfzV4gGNeRQyVrY7Uk59rX+c8tzz63j8DeZPLqmXvBxj8pA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!-- Carousel -->
+<link rel="stylesheet" href="{{ asset('webview/assets/carousel/css/docs.theme.min.css') }}">
+<link rel="stylesheet" href="{{ asset('webview/assets/carousel/owlcarousel/assets/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('webview/assets/carousel/owlcarousel/assets/owl.theme.default.min.css') }}">
+
+<!-- Carousel JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="{{ asset('webview/assets/carousel/owlcarousel/owl.carousel.js') }}"></script>
+
+<!-- Custom style -->
+<link rel="stylesheet" href="{{ asset('webview/assets/css/style.css') }}">
+
+<!-- Toastr -->
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+<!-- Inline styles -->
+<style>
+    .dropdown:hover>.dropdown-menu {
+        display: block !important;
+    }
+</style>
